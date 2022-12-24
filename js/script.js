5034efd19038e0e7e3cf76a2fb88dfc4928d27c0
TubiOb <tubiobaloluwa@gmail.com>
2022-12-24 23:58:17 +0100

    Commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// READ MORE - READ LESS FUNCTIONALITY
 function myFunction() {
     var dots = document.getElementsByClassName("dots");
     var moreText = document.getElementsByClassName("more");
@@ -15,3 +16,38 @@ function myFunction() {
         }
     }
 }
+
+
+
+// ADDING FUNCTIONALITY TO THE SEARCH ICON
+const searchButton = document.getElementById('search-button');
+
+searchButton.addEventListener('click', function() {
+    const searchQuery = window.prompt('Enter a search query:');
+
+    processSearch(searchQuery);
+});
+
+
+
+//LOGIN AND SIGNUP FORM TOGGLE
+const signupForm = document.getElementById('signup-form');
+const loginForm = document.getElementById('login-form');
+const signupLink = document.getElementById('signup-link');
+const loginLink = document.getElementById('login-link');
+
+signupLink.addEventListener('click', function(event) {
+    event.preventDefault(); // prevent the page from reloading
+    signupForm.style.display = "block";
+    loginForm.style.display = "none";
+    signupLink.style.display = "none";
+    loginLink.style.display = "inline";
+});
+
+loginLink.addEventListener('click', function(event) {
+    event.preventDefault(); // prevent the page from reloading
+    loginForm.style.display = "block";
+    signupForm.style.display = "none";
+    loginLink.style.display = "none";
+    signupLink.style.display = "inline";
+});
